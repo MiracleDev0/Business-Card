1eec96c37633a0ef369c7770eed9a3ca12843799
Esan Miracle <miracleesan7@gmail.com>
2024-05-13 12:20:33 +0100

    A little change

--- components/info.jsx
+++ components/info.jsx
@@ -3,7 +3,7 @@
 export default function Info() {
     return (
         <main>
-            <img src="./images/miracle.jpg" alt="" />
+            <img src="./images/miracle.png" alt="A picture of Esan Miracle" />
 
         </main>
     )
